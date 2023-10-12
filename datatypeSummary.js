3bf6d3239bfc8484783a812ff2e68824a7ebd1c6
Ehtisham <117318809+EhtishamShah00@users.noreply.github.com>
2023-10-12 05:21:08 +0500

    new file

---
+++ datatypeSummary.js
@@ -0,0 +1,16 @@
+//Primitive
+//Types 7: String, Number, Boolean, null, undefined, Symbol
+
+//String:
+const greating = "Hello";
+//Number:
+const age = 18; //it also can be 18.3 which is float
+//Boolean:
+const didyoueat = true;
+//null is empty
+const counttemp = null;
+//undefinded means we will assign the value later on.
+let didhearive;
+//Symbol: its used to keep data unique
+const id = Symbol("1, 2, 3");
+console.log(id);
