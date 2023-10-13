fac3c58bdb921f55e15ca93f1e7caffeb37ea6d0
Ehtisham <117318809+EhtishamShah00@users.noreply.github.com>
2023-10-13 05:53:39 +0500

    practice

--- 01-basics/praticeStrings.js
+++ 01-basics/praticeStrings.js
@@ -1,8 +1,63 @@
 const myName = "Ehtisham Shah";
-//at() method
-// console.log(myName.length);
-//I want to see the last character using index
-let index = 12;
-console.log(
-  `My index is ${index} and my name last character is ${myName.at(index)}`
-); //Answer will be h
+const namePart = "Shah";
+//IMP NOTE INDEX MEANS CHARACTER PLACEHOLDERS LIKE 1 2 3 ON 1 THERE IS CHARACTER h and so on
+
+//TOTAL LENGHT IS 12
+
+/*..........................................*/
+
+//------at() method------//
+
+// I want to see the last character using index
+// let index = 12;
+// console.log(
+//   `My index is ${index} and my name last character is ${myName.at(index)}`
+// ); .... Answer will be h
+
+/*..........................................*/
+
+//------charAt() method------//
+
+// console.log(myName.charAt(7)); .... Answer will be m
+
+/*..........................................*/
+
+//------endsWith() method------//
+
+//it return true or false if the strings end with the string you have given it.
+
+// console.log(myName.endsWith("Shah")); ...Answer is true
+
+/*..........................................*/
+
+//------includes() method------//
+
+//it return true or false if the strings have the string that you have given it.
+
+// console.log(myName.includes("Shah")); ...Answer is true
+
+/*..........................................*/
+
+//------includes() method------//
+
+//it return true or false if the strings have the string that you have given it.
+
+// console.log(myName.includes(namePart)); ...Answer is true
+
+/*..........................................*/
+
+//------indexOf() method------//
+
+//it returns the placeholder of words of the string by using the word you have given it like i want the index of "m" etc.
+
+// console.log(myName.indexOf("m")); ...Answer is 7
+
+/*..........................................*/
+
+//------lastindexOf() method------//
+
+//it returns the last placeholder of words of the string by using the word you have given it like i want the last index of "h" etc.
+
+// console.log(myName.lastIndexOf("h")); ...Answer is 12
+
+/*..........................................*/
