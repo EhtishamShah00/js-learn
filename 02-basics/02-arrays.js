ada87ebf77b17ca86fed5cc4dee31fa569e7ae8c
Ehtisham <117318809+EhtishamShah00@users.noreply.github.com>
2023-10-16 05:11:06 +0500

    ARRAYS

---
+++ 02-basics/02-arrays.js
@@ -0,0 +1,30 @@
+// const marvel_heros = ["thor", "IronMan", "SpiderMan", "Hulk"];
+// const dc_heros = ["Batman", "SuperMan", "Flash", "Wonder Women"];
+// const concatHeros = marvel_heros.concat(dc_heros);
+// console.log(concatHeros);
+// const new_array = [...marvel_heros, ...dc_heros];
+// console.log(new_array);
+//Nested Array:
+// const nestedArr = [
+//   1,
+//   3,
+//   4,
+//   5,
+//   6,
+//   [1, 3, 5, 6],
+//   78,
+//   [22, 3, 444, 55, [3, 3, 2, 1]],
+// ];
+// console.log(nestedArr.flat(2));
+
+//Convert to array
+console.log(Array.isArray("EHTISHAM"));
+console.log(Array.from("123456"));
+
+const score1 = 100;
+const score2 = 100;
+const score3 = 100;
+
+console.log(Array.of(score1, score2, score3));
+
+console.log(Array.from({ name: "EHTISHAM" }));
