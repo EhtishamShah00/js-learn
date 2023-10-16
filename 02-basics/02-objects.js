7060cfa8b5a94aa74454b592df348c74dd0befa7
Ehtisham <117318809+EhtishamShah00@users.noreply.github.com>
2023-10-17 04:58:59 +0500

    objects

---
+++ 02-basics/02-objects.js
@@ -0,0 +1,74 @@
+// const fb = new Object();..................Singleton
+
+const fbUser = {};
+fbUser.userId = "123ab";
+fbUser.name = "Ehtisham";
+fbUser.isLoggedIn = false;
+fbUser.innerObject = {
+  innerId: "123334a",
+  innerName: "Ehtisham",
+};
+
+// console.log(fbUser);
+// console.log(fbUser.innerObject["innerName"]);
+
+// const userDetails = {
+//   email: "hello@gmail.com",
+//   fullName: { firstName: "Ehtisham", lastName: "Shah" },
+//   dataforReg: {
+//     date: "2022/02/22",
+//     regDate: {
+//       userReg: "2023/03/22",
+//     },
+//   },
+// };
+
+// console.log(userDetails.dataforReg.regDate["userReg"]);
+
+// // Combine Objects
+// const obj1 = { name: "Ehtisam" };
+// const obj2 = { isloggedIn: true };
+// // const combineObj = { ...obj1, ...obj2 };
+// //or
+// const combineObj = Object.assign({}, obj1, obj2);
+// console.log(combineObj);
+
+// const userdata = [
+//   {
+//     name: "Ehtisham",
+//     id: "123",
+//     email: "123@gmail.com",
+//   },
+//   {
+//     name: "Ehtisham",
+//     id: "123",
+//     email: "123@gmail.com",
+//   },
+//   {
+//     name: "Ehtisham",
+//     id: "123",
+//     email: "123@gmail.com",
+//   },
+//   {
+//     name: "Ehtisham",
+//     id: "123",
+//     email: "123@gmail.com",
+//   },
+//   {
+//     name: "Ehtisham",
+//     id: "123",
+//     email: "123@gmail.com",
+//   },
+//   {
+//     name: "Ehtisham",
+//     id: "123",
+//     email: "123@gmail.com",
+//   },
+// ];
+// // userdata.forEach((users) => console.log(users.name));
+// console.log(userdata[1].id);
+console.log(fbUser);
+console.log(Object.keys(fbUser));
+console.log(Object.values(fbUser));
+console.log(Object.entries(fbUser));
+console.log(fbUser.hasOwnProperty("names"));
