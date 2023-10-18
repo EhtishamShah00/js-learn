22774e44d62a50883459d2c990a7c0954f31728c
Ehtisham <117318809+EhtishamShah00@users.noreply.github.com>
2023-10-18 05:35:02 +0500

    block scope and hosting

--- 03-basics/scope.js
+++ 03-basics/scope.js
@@ -1,6 +1,30 @@
-let a = 10;
-const b = 20;
-var c = 30;
-console.log(a);
-console.log(b);
-console.log(c);
+// let d = 20;
+
+// if (true) {
+//   let a = 10;
+//   console.log(d);
+//   const b = 20;
+//   // var c = 30;
+// }
+//.................Var will be called outside the if statement even if its not delecared outside so dont use it
+// // console.log(b);
+// // console.log(c);
+
+// function one() {
+//   const num1 = 1;
+//   console.log(webiste);
+//   function two() {
+//     const website = "www.google.com";
+//     console.log(num1);
+//   }
+//   two();
+// }
+// one();
+
+if (true) {
+  const userName = "Ehtisham";
+  if (userName === "Ehtisham") {
+    const website = "www.google.com";
+    console.log(userName + website);
+  }
+}
